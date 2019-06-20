e121dc8cd6ea3bc203844fd57f607274c5d4b99d
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-06-20 10:34:52 -0400

    main.c file for testing

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf = ("\"Programming is like building a multilingual puzzle and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+    return (0);
+}
