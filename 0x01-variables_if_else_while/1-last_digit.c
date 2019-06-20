3639d4585cbc83e8ab11b907dc8e72524ae51e8c
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-06-20 13:36:55 -0400

    remove lastNum variable in task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,19 +12,18 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int lastNum = n % 10;
 
-	if (lastNum > 5)
+	if ((n % 10) > 5)
 	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, lastNum);
+	printf("Last digit of %d is %d and is greater than 5\n", n, (n % 10));
 	}
-	else if (lastNum == 0)
+	else if ((n % 10) == 0)
 	{
-	printf("Last digit of %d is %d and is 0\n", n, lastNum);
+	printf("Last digit of %d is %d and is 0\n", n, (n % 10));
 	}
 	else
 	{
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastNum);
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, (n % 10));
 	}
 	return (0);
 }
