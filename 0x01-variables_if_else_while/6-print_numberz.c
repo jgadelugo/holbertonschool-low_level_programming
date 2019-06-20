98d9a7c88b0e62998b146890b8473e2a615a5713
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-06-20 15:40:06 -0400

    task 6, print numbers not using char variable and using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int nums;
+
+for (nums = '0'; nums <= '9'; nums++)
+{
+putchar(nums);
+}
+putchar('\n');
+return (0);
+}
