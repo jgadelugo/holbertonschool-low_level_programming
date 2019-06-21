4830521fb92388de1a9a054695d05a7db710746d
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-06-20 20:44:05 -0400

    make int a char character in if statement

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int nums;
+
+for (nums = '0'; nums <= '9'; nums++)
+{
+putchar(nums);
+if (nums == '9')
+{
+continue;
+}
+else
+{
+putchar(' ');
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
