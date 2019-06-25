691d7b217dd4821f4c9874521d46f241a0cbca63
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-06-25 10:42:56 -0400

    modify task 0 to pass checker

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,10 +7,16 @@
  */
 int main(void)
 {
-char h[10] = "Holberton\n";
-
-for (int i = 0; i < 10; i++)
-_putchar(h[i]);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 
 return (0);
 }
