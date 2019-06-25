6893cf1a432ab64df15ecab8457a70cb9ec8970f
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-06-25 16:16:04 -0400

    task 10, add two values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - prints out the addition of two numbers
+ *@x: first value
+ *@y: second value
+ * Return sum of two numbers
+ */
+
+int add(int x, int y)
+{
+int i;
+i = x + y;
+return (i);
+}
