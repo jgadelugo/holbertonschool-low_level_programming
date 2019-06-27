486566d41aa105b1dd1f92754beed41b3e3b7e8d
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-06-27 13:32:35 -0400

    remove space at the end

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,13 +11,15 @@ int x;
 for (x = 1; x <= 100; x++)
 {
 if (x % 3 == 0 && x % 5 == 0)
-printf("FizzBuzz ");
+printf("FizzBuzz");
 else if (x % 3 == 0)
-printf("Fizz ");
+printf("Fizz");
 else if (x % 5 == 0)
-printf("Buzz ");
+printf("Buzz");
 else
-printf("%d ", x);
+printf("%d", x);
+if (x < 100)
+printf(" ");
 }
 putchar('\n');
 return (0);
