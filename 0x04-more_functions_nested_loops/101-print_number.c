15d3ef4ac2b225cfb308adea46466ce9baa84a58
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-06-27 14:25:39 -0400

    increase range it can print

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -32,4 +32,12 @@ void print_number(int n)
 		_putchar((n / 10) % 10 + '0');
 		_putchar(n % 10 + '0');
 	}
+		else if (n < 100000)
+	{
+		_putchar((n / 10000) + '0');
+		_putchar((n / 1000) % 10 + '0');
+		_putchar((n / 100) % 10 + '0');
+		_putchar((n / 10) % 10 + '0');
+		_putchar(n % 10 + '0');
+	}
 }
