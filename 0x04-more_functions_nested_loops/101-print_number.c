cc1f3bf65b0d5bc25a758020fc95b80e51d252a9
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-06-27 21:41:13 -0400

    went back to missing one check

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,19 +7,15 @@
 
 void print_number(int n)
 {
-	int count, x, y, k;
+	int count, x, y;
 
 	x = 0;
 	y = 1000000000;
 
-	k = n % 10;
-	if (k < 0)
-		k *= -1;
 	if (n == 0)
 		_putchar('0');
 	else if (n < 0)
 	{
-		n /= 10;
 		n *= -1;
 		_putchar('-');
 	}
@@ -35,10 +31,8 @@ void print_number(int n)
 			_putchar((n / y) + '0');
 			x += 1;
 		}
-		else if (y > 1)
-			_putchar(((n / y) % 10) + '0');
 		else
-			_putchar(k + '0');
+			_putchar(((n / y) % 10) + '0');
 		y /= 10;
 	}
 }
