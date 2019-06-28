0309186bc6ca9aff490fe2a3d770bd23d9d01de4
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-06-27 20:02:04 -0400

    decrease line count

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,43 +1,35 @@
 #include "holberton.h"
 
 /**
- * print_number - prints an integer
- * @n: variable to be printed
+ * print_number - print any integer using putchar
+ * @n: integer to be printed
  */
 
 void print_number(int n)
 {
-	if (n < 0)
-	{
-		_putchar('-');
+	int count, x, y;
+
+	x = 0;
+	y = 1000000000;
+
+	if (n == 0)
+		_putchar('0');
+	else if (n < 0)
 		n *= -1;
-	}
-	if (n < 10)
-		_putchar(n + '0');
-	else if (n < 100)
+	for (count = 0; count < 10; count++)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-	else if (n < 1000)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar((n % 10) + '0');
-	}
-	else if (n < 10000)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-		else if (n < 100000)
-	{
-		_putchar((n / 10000) + '0');
-		_putchar((n / 1000) % 10 + '0');
-		_putchar((n / 100) % 10 + '0');
-		_putchar((n / 10) % 10 + '0');
-		_putchar(n % 10 + '0');
+		if (n / y == 0 && x == 0)
+		{
+			y /= 10;
+			continue;
+		}
+		else if (x == 0)
+		{
+			_putchar((n / y) + '0');
+			x += 1;
+		}
+		else
+			_putchar(((n / y) % 10) + '0');
+		y /= 10;
 	}
 }
