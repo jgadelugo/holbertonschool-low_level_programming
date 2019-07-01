ada7161f90d1c73c83fda0094f331847b6d90fe9
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-01 12:10:09 -0400

    task 0, change value of pointed value to 98

---
+++ 0x05-pointers_arrays_stirngs/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - reset value pointer points to to 98
+ * @*n: pointer whose value will be changed
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_stirngs/holberton.h
+++ 0x05-pointers_arrays_stirngs/holberton.h
@@ -2,5 +2,6 @@
 #define HOLBERTON_H
 
 int _putchar(char c);
+void reset_to_98(int *n);
 
 #endif /*HOLBERTON_H*/
