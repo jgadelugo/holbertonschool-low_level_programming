bf197a40d87d57a4a202ba22818c78a814d34da0
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-02 10:23:36 -0400

    task 7, fix for odd case

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,8 +14,8 @@ void puts_half(char *str)
 		i++;
 	half = (i) / 2;
 	if (i % 2 != 0)
-		half = (i - 1) / 2;
-	for (; half <= i; half++)
+		half = ((i - 1) / 2) + 1;
+	for (; half < i; half++)
 		_putchar(str[half]);
 	_putchar('\n');
 }
