8bccd22bb43be1547e040d18b238efafef1e42eb
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-02 11:38:37 -0400

    fix negative

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,7 +14,7 @@ int _atoi(char *s)
 	x = 0;
 	numcount = 0;
 	mult = 1;
-	neg = 0;
+	neg = 1;
 	num = 0;
 
 	while (s[len] != '\0')
@@ -35,9 +35,7 @@ int _atoi(char *s)
 	for (; x <= len; x++)
 	{
 		if (s[x] == '-')
-			neg += -1;
-		else if (s[x] == '+')
-			neg += 1;
+			neg *= -1;
 		else if (s[x] <= '9' && s[x] >= '0')
 		{
 			num += (s[x] - '0') * mult;
