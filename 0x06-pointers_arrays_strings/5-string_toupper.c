778bc49ca34607c2281200cd4a72b2c1c09b7c2b
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-05 14:37:52 -0400

    task 5, uppercase all charachters

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * string_toupper - convert to uppercase
+ * @a: string in lowercase
+ * Return: uppercase
+ */
+
+char *string_toupper(char *a)
+{
+	int i;
+
+	for (; *(a + i) != '\0'; i++)
+	{
+		if (*(a + i) >= 'a' && *(a + i) <= 'z')
+			*(a + i) -= 32;
+	}
+	return (a);
+}
