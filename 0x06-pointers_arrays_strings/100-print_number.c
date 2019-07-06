3bfaaa03db21eb2ab5d5cd0224353b6095986c75
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-06 16:32:48 -0400

    task 9, used code that worked before

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,40 +1,42 @@
 #include "holberton.h"
 
 /**
- * print_number - prints an integer using putchar
+ * print_number - print any integer using putchar
  * @n: integer to be printed
  */
 
 void print_number(int n)
 {
-	int mult, x, k;
+	int count, x, y, k;
 
 	x = 0;
-	mult = 1;
+	y = 1000000000;
 
 	if (n == 0)
 		_putchar('0');
-	else if ( n > 0)
+	else if (n > 0)
 		n *= -1;
 	else
 		_putchar('-');
-
-	while (n / mult != 0)
-		mult *= 10;
-	mult /= 10;
-	for (; mult > 0; mult /= 10)
+	for (count = 0; count < 10; count++)
 	{
-		if (x == 0)
+		if (n / y == 0 && x == 0)
 		{
-			_putchar(-(n / mult) + '0');
-			x++;
+			y /= 10;
+			continue;
+		}
+		else if (x == 0)
+		{
+			_putchar(-(n / y) + '0');
+			x += 1;
 		}
 		else
 		{
-			k = (-(n / mult) % 10);
+			k = (-(n / y) % 10);
 			if (k < 0)
 				k *= -1;
 			_putchar(k + '0');
 		}
+		y /= 10;
 	}
 }
