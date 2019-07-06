dabb1143cf64d473754d6763682d727ef51cecbb
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-06 12:51:36 -0400

    task 3, correct if statement

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,7 +14,7 @@ int _strcmp(char *s1, char *s2)
 
 	while (*(s1 + count) != '\0' || *(s2 + count) != '\0')
 	{
-		if (s1 != s2)
+		if (*(s1 + count) != *(s2 + count))
 			return (*(s1 + count) - *(s2 + count));
 		count++;
 	}
