d47737eb89c4d118ac150255bda5f9adf83579e8
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-06 15:36:13 -0400

    task 8, encode and decode rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -11,12 +11,15 @@ char *rot13(char *a)
 {
 	int i, x, hold;
 	char check[] = "AaZz";
-	char ntom[] = "nopqrstuvwxyzabcdefghijklm";
-
-	for (x = 0; (*(a + i) >= check[x] && *(a + i) <= check[x + 2]); x++)
-	{
-		hold = (*(a + i) - 65 - (x * 32));
-		*(a + i) = (ntom[hold] - (x * 32));
-	}
+	char ntom[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
+	for (i = 0; *(a + i) != '\0'; i++)
+		for (x = 0; x < 2; x++)
+		{
+			if (*(a + i) >= check[x] && *(a + i) <= check[x + 2])
+			{
+				hold = (*(a + i) - 65 - (x * 32));
+				*(a + i) = (ntom[hold] + (x * 32));
+			}
+		}
 	return (a);
 }
