f0edd8fd4b0af3334e19137317509f46eb6bf1f3
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-09 17:17:34 -0400

    testing

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,9 +1,12 @@
-
+#include "holberton.h"
 
 /**
  * set_string - function that sets the value of a pointer to a char
  * @s: value to overide
  * @to: value to copy
+ *
+ *
+ *
  */
 void set_string(char **s, char *to)
 {
