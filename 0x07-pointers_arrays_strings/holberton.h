384cb54aca493030dafbefe9ddd157995631a3ec
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-09 17:24:38 -0400

    task 8 holberton file

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -9,5 +9,6 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
+void set_string(char **s, char *to);
 
 #endif /*HOLBERTON_H*/
