283f5c9a615ff5c253da8868669f9456d9469832
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-09 15:52:56 -0400

    task 6, betty style fixed

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,15 +1,14 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
- *
+ * print_chessboard - print chessboard
+ * @a: 8 by 8 array
  */
 
 void print_chessboard(char (*a)[8])
 {
 	int x, i = 0;
+
 	for (x = 0; x < 8; x++)
 	{
 		_putchar(a[i][x]);
