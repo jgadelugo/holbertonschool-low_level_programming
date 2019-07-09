23c3a711045756ac05a4d9cb929fdb99240e99c6
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-09 13:31:19 -0400

    task 2, strchr function

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer where we search for charachter
+ * @c: character we search for
+ * Return: NULL if character is not found, return pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *s1 = s;
+
+	for (; *s1 != '\0'; s1++)
+		if (*s1 == c)
+			return (s1);
+	return ('\0');
+}
