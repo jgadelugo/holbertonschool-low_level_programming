85d16133b9a111bd60ebf20962d1ea20e668d088
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-10 14:26:33 -0400

    fix style

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,12 +3,12 @@
 /**
  * _strlen_recursion - get length of string
  * @s: pointer to a string
+ * Return: length of string
  */
 
 int _strlen_recursion(char *s)
 {
-
 	if (*s == '\0')
 		return (0);
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
