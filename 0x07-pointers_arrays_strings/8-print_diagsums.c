bd843340761451b449e4e71ecf170d7349329059
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-09 20:10:02 -0400

    fix styling to comply with betty

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
+ * print_diagsums - add diagonal lines of a square matrix
+ * @a: square matrix
+ * @size: size of matrix
  */
 
 void print_diagsums(int *a, int size)
 {
-	
 	int lfrg = 0, rglf = size - 1;
 	int valuelfrg = 0, valuerglf = 0;
 	int *p = a;
-	
+
 	for (; lfrg <= size * size; lfrg += (size + 1), rglf += (size - 1))
 	{
 		valuelfrg += *(p + lfrg);
@@ -21,5 +20,3 @@ void print_diagsums(int *a, int size)
 	}
 	printf("%d, %d\n", valuelfrg, valuerglf);
 }
-	
-	
