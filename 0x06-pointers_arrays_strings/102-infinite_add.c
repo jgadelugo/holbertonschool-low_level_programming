b1d467f6bcbb392cbd308c33f0ad039742386bd3
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-10 13:45:16 -0400

    update

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,7 @@
+#include "holberton.h"
+
+/**
+ *
+ *
+ */
+
--- 0x08-puts_recursion.c/holberton.h
+++
@@ -1,8 +0,0 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-int _putchar(char c);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-
-#endif /*HOLBERTON_H*/
