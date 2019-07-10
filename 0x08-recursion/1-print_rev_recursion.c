6a881a627d661beaa1b30bfe3a0db6e7d23c1a4a
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-10 14:12:30 -0400

    task 1, not using static variables

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,38 +1,15 @@
 #include "holberton.h"
 
 /**
- * get_length - gets length and prints charachters in reverse
- * @s: pointer to a string
- * @len: length of strin
- * @check: checks that it got to the end
- */
-void get_length(char *s, int len, int check)
-{
-	if (*(s + len) != '\0' && check == 0)
-	{
-		len++;
-		get_length(s, len, check);
-	}
-	else if (len > 0)
-	{
-
-		check++;
-		_putchar(*(s + len));
-		len--;
-		get_length(s, len, check);
-	}
-	else
-		_putchar('\n');
-}
-
-/**
  * _print_rev_recursion - prints a string in reverse
  * @s: pointer to a string
  */
 
 void _print_rev_recursion(char *s)
 {
-	int len = 0, check = 0;
 
-	get_length(s, len, check);
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
