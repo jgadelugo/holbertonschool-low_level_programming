4fd5a11a191e845e5ba9c1687eca1e8ead3fb696
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-12 16:10:14 -0400

    fix style

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * chartoint - function that checks array of characters for integers
+ * @c: array of characters
+ * Return: 0 if successful, -1 if fails
+ */
+int chartoint(char *c)
+{
+	if (*c == '0')
+		return (0);
+	else if (atoi(c) == 0)
+		return (-1);
+	return (atoi(c));
+}
+/**
+ * main - function that multiplies two numbers
+ * @argc: the size of the argv array, the number of command line arguments
+ * @argv: an array containing the program command line arguments
+ * Return: 0 if successful
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1, sum = 0; i < argc; i++)
+	{
+		if (chartoint(argv[i]) > -1)
+			sum += chartoint(argv[i]);
+		else
+		{
+			printf("ERROR\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
+
