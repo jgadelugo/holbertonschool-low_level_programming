95a8406aaa9ad57c28a55d2d199f03c642fef41a
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-12 11:26:02 -0400

    task 3, multiply two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - function that multiplies two numbers
+ * @argc: the size of the argv array, the number of command line arguments
+ * @argv: an array containing the program command line arguments
+ * Return: 0 if successful 
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (0);
+	}
+	int x = atoi(argv[1]);
+	int y = atoi(argv[2]);
+	int mul = x * y;
+	printf("%d\n", mul);
+	return (0);
+}
