0e32af38ffbeee093f49996574fa0850041a4c51
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-12 10:59:33 -0400

    task 0, print name of executable

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints its name followed by a new line 
+ * @argc: the size of the argv array, the number of command line arguments
+ * @argv: an array containing the program command line arguments
+ * Return: 0 if successful 
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
