ba0bf58121c9809ac0b6393e702f57ef918f54be
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-12 10:59:51 -0400

    task 1, print number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints number of arguments followed by a new line 
+ * @argc: the size of the argv array, the number of command line arguments
+ * @argv: an array containing the program command line arguments
+ * Return: 0 if successful 
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
