8ae80a00cef2a7d11f17b321a25b18c1cd7a3d51
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-14 16:00:55 -0400

    adjust variable declaration

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,20 +2,24 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
+ * main - print min number of coins to return change using 25, 10, 5, 2, 1
+ * @argc:  the size of the argv array, the number of command line arguments
+ * @argv: an array containing the program command line arguments
+ * Return: 1 if error 0 if success
  */
 
 int main(int argc, char *argv[])
 {
+	int cents, remainder, coins;
+	int quarters, dimes, nickles, twos, pennies;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int cents = atoi(argv[1]), remainder, coins;
-	int quarters, dimes, nickles, twos, pennies;
+	else
+		cents = atoi(argv[1]);
 
 	if (cents <= 0)
 	{
