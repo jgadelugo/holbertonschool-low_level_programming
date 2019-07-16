33bb69a2734ea407f1b698d113a4ca62f1208cb4
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-16 14:20:30 -0400

    test

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,12 +12,15 @@ char *_strdup(char *str)
 	char *ptr;
 	unsigned int x, len;
 
+	if (!str)
+		return (NULL);
+
 	for (len = 0; str[len]; len++)
 		;
 	len++;
 	ptr = malloc(len * sizeof(char));
 
-	if (ptr == 0 || str == 0)
+	if (!ptr)
 		return (NULL);
 
 	for (x = 0; x <= len; x++)
