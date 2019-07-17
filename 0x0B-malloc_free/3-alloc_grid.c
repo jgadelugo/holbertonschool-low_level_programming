d1bedf79c20065a5dcdeefd339a469635154a7b8
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-16 23:05:55 -0400

    sizeof *int

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	grid = (int **)malloc(height * sizeof(int));
+	grid = (int **)malloc(height * sizeof(int *));
 
 	if (!grid)
 	{
