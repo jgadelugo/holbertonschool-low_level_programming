b30cc1c42af7c2d698baaa101045fff505824bc2
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-16 23:18:33 -0400

    remove new for loop to set values

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -34,10 +34,6 @@ int **alloc_grid(int width, int height)
 			free(grid);
 			return (NULL);
 		}
-	}
-
-	for (row = 0; row < height; row++)
-	{
 		for (col = 0; col < width; col++)
 			grid[row][col] = 0;
 	}
