d0603469824186af5505f4b5bedb6e1c0df97e42
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-16 22:30:21 -0400

    task 3, function that returns a pointer to a 2 dimensional array of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: rows
+ * @height: columns
+ * Return: a pointer to a 2 dimensional array of integers
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, row, col;
+
+	if (width < 1 || height < 1)
+		return (NULL);
+	grid = (int **)malloc(height * sizeof(int));
+
+	if (!grid)
+		return (NULL);
+
+	for (row = 0; row < height; row++)
+	{
+		grid[row] = (int *)malloc(width * sizeof(int));
+		for (col = 0; col < width; col++)
+			grid[row][col] = 0;
+	}
+	return (grid);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -5,5 +5,6 @@ int _putchar(char c);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
 
 #endif /*HOLBERTON_H*/
