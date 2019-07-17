e2706af1120e030777af22ae9e36d690d411459b
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-17 13:50:20 -0400

    task 5, concatenate argv

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,6 +1,23 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+/**
+ * _strlen - get length
+ * @c: string
+ * Return: returns length
+ */
+
+int _strlen(char *c)
+{
+	int len;
+
+	for (len = 0; c[len] != '\0'; len++)
+		;
+
+	return (len);
+}
 
 /**
  * argstostr - concat str
@@ -11,31 +28,28 @@
 
 char *argstostr(int ac, char **av)
 {
-	char **ptr;
-	int x, y, len, start = 0;
+	char *ptr;
+	int x, y, len, c;
 
 	if (!ac || !av)
 		return (NULL);
-
-	ptr = (char **)malloc((ac) * sizeof(char *));
+	for (x = 0; x < ac; x++)
+	{
+		len += _strlen(av[x]);
+	}
+	ptr = (char *)malloc((len) * sizeof(char *));
 
 	if (!ptr)
 		return (NULL);
 
 	for (x = 0; x < ac; x++)
 	{
-		len = strlen(av[x]);
-
-		ptr[x] = (char *)malloc(len + 1 * sizeof(char));
-
-		if (!ptr[x])
-			return (NULL);
-		for (y = 0; y <= len; y++)
+		for (y = 0; av[x][y]; y++, c++)
 		{
-			ptr[x][y] = av[x][y];
+			ptr[c] = av[x][y];
 		}
-		ptr[x][start + y] = '\n';
-		start += len;
+		ptr[c] = '\n';
+		c++;
 	}
-	return (*ptr);
+	return (ptr);
 }
