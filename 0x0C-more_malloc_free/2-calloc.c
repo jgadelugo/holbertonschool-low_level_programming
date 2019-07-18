5199ee8d9ca7930c5518b6152cc639ce59f24d99
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-18 14:02:12 -0400

    fix condition in for loop

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -20,7 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (!ptr)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		((char *) ptr)[i] = 0;
 
 	return (ptr);
