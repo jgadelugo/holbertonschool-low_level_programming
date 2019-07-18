8b8fe80a2bd50ae5c14e630e77c6bdb4dda27fba
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-18 16:50:53 -0400

    less than or equal to old size

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -30,7 +30,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	}
 
-	if (new_size == old_size)
+	if (new_size <= old_size)
 		return (ptr);
 
 	ptr1 = malloc(new_size);
