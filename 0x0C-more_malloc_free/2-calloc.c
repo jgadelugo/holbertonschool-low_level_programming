e73d0eb931405d51c3b117c4dba8eaddd9e9aca0
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-18 11:40:59 -0400

    allocate memory for void pointer

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+
+/**
+ * _calloc - function that allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of type
+ * Return: void pointer if succesful return NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int i;
+
+	if (!nmemb || !size)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+
+	if (!ptr)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		((char *) ptr)[i] = 0;
+
+	return (ptr);
+}
