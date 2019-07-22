f7589e6843a42fb926830c185b55acb3ce1069ca
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-22 16:14:23 -0400

    use variables

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -8,12 +8,17 @@
 
 void print_dog(struct dog *d)
 {
+	char *name, *owner;
+
 	if (d)
 	{
-		if (!d->name)
-			d->name = "(nill)";
-		if (!d->owner)
-			d->owner = "(nill)";
-		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+		name = d->name;
+		owner = d->owner;
+
+		if (!name)
+			name = "(nill)";
+		if (!owner)
+			owner = "(nill)";
+		printf("Name: %s\nAge: %f\nOwner: %s\n", name, d->age, owner);
 	}
 }
