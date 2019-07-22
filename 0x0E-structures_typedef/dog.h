d95c6f52d767e1da71ff61414608af4445746a46
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-22 13:13:23 -0400

    description for struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+
+/**
+ * struct dog - struct with elements name, age, owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: struct for dog
+ */
 struct dog
 {
 	char *name;
