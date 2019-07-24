f3a04b242c2e9f99388c87309538e276fcffc8f2
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-24 12:37:56 -0400

    task 1, execute an array of elements in a function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function that executes a function given as a parameter
+ * @array: array of parameters to execute
+ * @size: size of the array
+ * @action: pointer to a function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && action && size)
+		for (size_t i = 0; i < size; i++)
+			action(array[i]);
+}
