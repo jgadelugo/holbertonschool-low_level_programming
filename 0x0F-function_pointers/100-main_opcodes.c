c37565de4afa291eb68f401da684a6a6a5b839a4
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-24 23:26:03 -0400

    task 4, program that prints the opcodes of its own main function

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the opcodes of its own main function
+ * @argc: number of elements in argv
+ * @argv: arguments
+ * Return: 0 if succesful
+ */
+
+int main(int argc, char *argv[])
+{
+	int arg, i;
+	unsigned char *pf;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	arg = atoi(argv[1]);
+	if (arg < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	pf = (unsigned char *)main;
+	for (i = 0; i < arg; i++)
+	{
+		printf("%x ", pf[i]);
+	}
+	printf("\n");
+	return (0);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,7 +1,7 @@
 Project 0x0F - C - Function Pointers
 
 Task 0 - write a function that prints a name
-Task 1 - 
-Task 2 - 
-Task 3 - 
-Task 4 - 
+Task 1 - function that executes a function given as a parameter on each element of an array
+Task 2 - function that searches for an integer
+Task 3 - calculator 
+Task 4 - program that prints the opcodes of its own main function
