c31f2aae728655cbc75bd86d6935d1445d7da215
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-25 17:56:59 -0400

    task 11, print the largest prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - print prime numbers
+ * main - print largest prime factor
  * Return: 0 if succesful
  */
 
 int main(void)
 {
-	int x;
-	
-	printf("%d\n", x);
+	long int i, j, lp, num = 612852475143;
+	int prime;
+
+	for (i = 2; i < num / 2; i++)
+	{
+		prime = 1;
+		if (num % i == 0)
+			lp = num / i;
+
+		for (j = 2; j < lp / 2; j++)
+			if (lp % j == 0)
+			{
+				prime = 0;
+				break;
+			}
+		if (prime)
+		{
+			printf("%ld\n", lp);
+			break;
+		}
+	}
+	return (0);
 }
