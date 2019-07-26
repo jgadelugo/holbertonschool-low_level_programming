95f728dd374c15520b301d479616502d97c0aa99
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-26 10:38:30 -0400

    task 1, print numbers with a separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: number of parameters
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	if (!n)
+		return;
+
+	va_start(valist, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(valist, int));
+		if (i != n - 1 && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+
+	va_end(valist);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,5 +3,6 @@
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif /*VARIADIC_FUNCTIONS*/
