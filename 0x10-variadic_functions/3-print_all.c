b72a555a85189f1c05d04f56fdd4ff0082dd15ed
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-28 20:39:25 -0400

    bug fix, only 2 if statements

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,48 +10,43 @@
 void print_all(const char * const format, ...)
 {
 	va_list valist;
-	unsigned int i = 0, n = 0;
-	char * arg;
+	unsigned int i = 0, middle = 0;
+	char *arg;
 
-	while (format[n])
-		n++;
 	va_start(valist, format);
 
-	while (i < n)
+	while (format[i])
 	{
+		if (middle)
+			printf(", ");
 		switch (format[i])
 		{
 		case 'c':
-			if (i)
-				printf(", ");
 			printf("%c", va_arg(valist, int));
 			break;
 		case 'i':
-			if (i)
-				printf(", ");
 			printf("%i", va_arg(valist, int));
 			break;
 		case 'f':
-			if (i)
-				printf(", ");
 			printf("%f", va_arg(valist, double));
 			break;
 		case 's':
-			if (i)
-				printf(", ");
 			arg = va_arg(valist, char *);
 			if (arg)
+			{
 				printf("%s", arg);
-			else
-				printf("%p", arg);
+				break;
+			}
+			printf("%p", arg);
 			break;
 		default:
+			middle = 0;
 			i++;
 			continue;
 		}
+		middle = 1;
 		i++;
 	}
 	printf("\n");
-
 	va_end(valist);
 }
