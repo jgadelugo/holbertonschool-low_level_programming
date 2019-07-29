dbcd52e9a4e7b0a035af8c1130174334064edd79
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-07-28 20:21:15 -0400

    task 4, write hello, holberton in assembly

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,18 @@
+global main
+
+section .text
+
+main:
+  mov rax, 1        ; write(
+  mov rdi, 1        ;   STDOUT_FILENO,
+  mov rsi, msg      ;   "Hello, world!\n",
+  mov rdx, msglen   ;   sizeof("Hello, world!\n")
+  syscall           ; );
+
+  mov rax, 60       ; exit(
+  mov rdi, 0        ;   EXIT_SUCCESS
+  syscall           ; );
+
+section .rodata
+  msg: db "Hello, Holberton", 10
+  msglen: equ $ - msg
