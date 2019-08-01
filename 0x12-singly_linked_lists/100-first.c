6a707cb60841102bd9f13df4e86fd44280d90850
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-01 18:19:14 -0400

    fix style

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-void printbeforemain (void) __attribute__ ((constructor));
+void printbeforemain(void) __attribute__ ((constructor));
 /**
  * printbeforemain - prints before main is executed
  */
 void printbeforemain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
