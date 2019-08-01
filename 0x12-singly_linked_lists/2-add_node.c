bc947206a7d56bf5355644cab24f48c20cb1edb6
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-01 15:17:37 -0400

    task 2, add a node to the beginning og a list_t

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - size of string
+ * @str: string to get size of
+ * Return: size of string
+ */
+int _strlen(const char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+		;
+
+	return (i);
+}
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: struct list_t, first node
+ * @str: needs to be duplicated
+ * Return: number of nodes
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = (char *)str;
+	new->len = _strlen(str);
+	new->next = *head;
+
+	*head = new;
+	return (new);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -22,5 +22,6 @@ typedef struct list_s
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif /*LISTS_H*/
