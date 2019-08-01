fcd2c796d668759f3f096f0a660d573302b0e212
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-01 15:53:09 -0400

    check string return of strdup

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,7 +21,7 @@ int _strlen(const char *str)
  * add_node - adds a new node at the beginning of a list_t list
  * @head: struct list_t, first node
  * @str: needs to be duplicated
- * Return: number of nodes
+ * Return: new list_t
  */
 
 list_t *add_node(list_t **head, const char *str)
@@ -29,10 +29,15 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new;
 	char *string = strdup(str);
 
-	new = malloc(sizeof(list_t));
-	if (!new)
+	if (!string)
 		return (NULL);
 
+	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		free(string);
+		return (NULL);
+	}
 	new->str = string;
 	new->len = _strlen(string);
 	new->next = *head;
