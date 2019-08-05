025622aba5d417d56bb30ae5cb1303a5b19b6c6f
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-05 13:25:59 -0400

    task 6, delete first node and retrieve its value

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * pop_listint - deleted the head node of a linked list and returns the (n)
+ * @head: double pointer to the linked list start
+ * Return: deleted value - n
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *hold;
+	int n;
+
+	hold = *head;
+	n = (**head).n;
+	*head = (**head).next;
+	free(hold);
+	return (n);
+}
