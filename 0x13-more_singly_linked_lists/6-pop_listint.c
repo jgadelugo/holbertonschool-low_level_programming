5aaed9f349a1548bed7a838760a9ffffed891627
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-05 17:15:47 -0400

    check if head is NULL

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,9 +12,13 @@ int pop_listint(listint_t **head)
 	listint_t *hold;
 	int n;
 
-	hold = *head;
-	n = (**head).n;
-	*head = (**head).next;
-	free(hold);
-	return (n);
+	if (head && *head)
+	{
+		hold = *head;
+		n = (**head).n;
+		*head = (**head).next;
+		free(hold);
+		return (n);
+	}
+	return (0);
 }
