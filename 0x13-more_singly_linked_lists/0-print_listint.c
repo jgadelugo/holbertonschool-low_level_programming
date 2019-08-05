7e79ca36d8ffeaa536b6f80676751cca4f081798
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-05 13:22:16 -0400

    fix style

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - print the elements of listint_t list and return # of elements
+ * print_listint - print the elements of linked list and return # of elements
  * @h: pointer to the first element
  * Return: number of elements
  */
