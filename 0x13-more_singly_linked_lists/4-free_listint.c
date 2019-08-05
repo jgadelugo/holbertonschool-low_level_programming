5aca4cccad3064fcca43c7a59b06b08191e962f0
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-05 12:28:08 -0400

    task 4, free linked list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint - function that frees listint_t list
+ * @head: pointer to the beginning of the linked list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *hold;
+	
+	while (head)
+	{
+		hold = head;
+		head = (*head).next;
+		free(hold);
+	}
+}
