44fa86170dedb3dd44e7ce3f567dce883fd827eb
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-05 12:41:56 -0400

    task 5, free list_t/ linked list using double pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - function that frees listint_t list
+ * @head: double pointer to the beginning of the linked list
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *hold;
+	
+	while (*head)
+	{
+		hold = *head;
+		*head = (**head).next;
+		free(hold);
+	}
+}
