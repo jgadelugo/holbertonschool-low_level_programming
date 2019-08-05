b909c7c5e461103f5836f2c7acc46934792d83cd
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-05 11:01:22 -0400

    task 0, print all the elements of a list and return the numbers

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_listint - print the elements of listint_t list and return # of elements
+ * @h: pointer to the first element
+ * Return: number of elements
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	for (i = 0; h; h = (*h).next, i++)
+		printf("%d\n", (*h).n);
+
+	return (i);
+}
