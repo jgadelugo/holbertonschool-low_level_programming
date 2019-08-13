e037c7d4d8c4dac12ef441340d94b18aefcc3fb5
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-13 19:02:43 +0000

    fix style

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -37,7 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buff);
 	close(of);
 
-	if(lRead > letters)
+	if (lRead > letters)
 		return (0);
-	return(lRead);
+	return (lRead);
 }
