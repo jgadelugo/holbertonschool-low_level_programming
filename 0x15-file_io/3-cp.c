855e0510b18e952b7056998aa431213a1bb1d5cb
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-08-14 23:40:17 +0000

    use ssize_t instead of int

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,27 +11,25 @@
 
 int main(int argc, char **argv)
 {
-	int bytes = 1024, lenRead = 1, lenWrite, checkClose, f1, f2;
+	int bytes = 1024, f1, f2;
+	ssize_t lenRead = 1, lenWrite;
 	char buffer[1024];
 
 	if (argc != 3)
 		exit(97);
-	f1 = open(argv[1], O_RDONLY);
-	if (f1 == -1)
+	while ((f1 = open(argv[1], O_RDONLY)) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+	exit(98);
 	}
-	f2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	if (f2 == -1)
+	while ((f2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664)) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[2]);
 		exit(98);
 	}
 	while (lenRead)
 	{
-		lenRead = read(f1, buffer, bytes);
-		if (lenRead == -1)
+		while ((lenRead = read(f1, buffer, bytes)) == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
@@ -43,17 +41,9 @@ int main(int argc, char **argv)
 			exit(99);
 		}
 	}
-	checkClose = close(f1);
-	if (checkClose == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
-		exit(100);
-	}
-	checkClose = close(f2);
-	if (checkClose == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
-		exit(100);
-	}
+	if (close(f1) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1), exit(100);
+	if (close(f2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2), exit(100);
 	return (0);
 }
