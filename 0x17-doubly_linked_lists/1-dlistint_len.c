625b702e6303d932ee8bd9e556928059a10de6cb
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-09-13 16:42:35 -0400

    [TASK] Return the number of elements in a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * _dlistint_len - count all the elements of a dlistint_t list 
+ * @h: doubly linked list node
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t size = 0;
+
+    for (; h; h = h->next, size++)
+        ;
+    return (size);
+}
