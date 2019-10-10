68c952a16ca502024c7aac1f4458bf9a52563124
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-10-10 20:53:26 +0000

    [TASK] Create a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_hash_table = malloc(sizeof(hash_table_t));
-	
+
 	if (!new_hash_table)
 		return (NULL);
 
@@ -23,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(new_hash_table);
 		return (NULL);
 	}
-	
+
 	for (; size; size--)
 		new_hash_table->array[size - 1] = NULL;
 
