060361afb9fe93e6f2426523c047de44508d5091
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-10-11 21:47:21 +0000

    [BUGFIX] Check if input is NULL

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,7 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *node;
+
 	if (!ht)
 		return (NULL);
 	node = ht->array[key_index((const unsigned char *)key, ht->size)];
