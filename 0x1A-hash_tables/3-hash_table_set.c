22df135b660f5ebce0768bb702f1be3f8ad486f7
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-10-11 21:20:16 +0000

    [BUGFIX] Return 0 if malloc fails when trying to update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -80,7 +80,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	
+
 	update = update_key(&(ht->array[index]), key, value);
 	/* update == 0 when no key was found */
 	if (update == 0)
