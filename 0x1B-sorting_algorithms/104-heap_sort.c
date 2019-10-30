3ac2489ede7ca55204b577450c970d9476046707
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-10-30 19:20:44 -0400

    [TASK] Create file

---
+++ 0x1B-sorting_algorithms/104-heap_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * heap_sort - sort array using heap sort
+ * @array: array to sort
+ * @size: size of array
+ */
+void heap_sort(int *array, size_t size)
+{
+	if (!array || !size)
+		return;
+}
---
+++ 0x1B-sorting_algorithms/105-radix_sort.c
@@ -0,0 +1,12 @@
+#include "sort.h"
+
+/**
+ * radix_sort - sort an array using radix sort
+ * @array: array to sort
+ * @size: size of array
+ */
+void radix_sort(int *array, size_t size)
+{
+	if (!array || !size)
+		return;
+}
