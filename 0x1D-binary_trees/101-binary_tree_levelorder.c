a6841f3047322f12c910beeb32ff3e716c9e551d
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-12-05 18:37:41 -0500

    [TASK] Print using level-order traversal

--- 0x1D-binary_trees/101-binary_tree_levelorder.c
+++ 0x1D-binary_trees/101-binary_tree_levelorder.c
@@ -1,16 +1,57 @@
 #include "binary_trees.h"
 
-
 /**
-  * binary_tree_delete - deletes a binary tree
-  * @tree: binary tree
+  * b_t_height - get height from binary tree
+  * @tree: tree to search
+  * Return: height
   */
-void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
+int b_t_height(const b_t_t *tree)
 {
+	int lSize = 0, rSize = 0;
+
+	if (!tree)
+		return (0);
 	else if (tree->left || tree->right)
 	{
-		binary_tree_nodes(tree->left);
-		binary_tree_nodes(tree->right);
+		lSize = b_t_height(tree->left);
+		rSize = b_t_height(tree->right);
+		if (lSize > rSize)
+			return (lSize + 1);
+		return (rSize + 1);
 	}
 	else
+		return (0);
+}
+/**
+ * print_level - prints one level at a time
+ * @tree: root of tree
+ * @func: function that prints
+ * @level: level to use func on
+ */
+void print_level(const b_t_t *tree, void (*func)(int), int level)
+{
+	if (!tree)
+		return;
+	if (level == 1)
+		func(tree->n);
+	else
+	{
+		print_level(tree->left, func, level - 1);
+		print_level(tree->right, func, level - 1);
+	}
+}
+/**
+ * binary_tree_levelorder - use function in levelorder traversal
+ * @tree: binary tree
+ * @func: function
+ */
+void binary_tree_levelorder(const b_t_t *tree, void (*func)(int))
+{
+	int lvl = 0, height;
+
+	if (!tree || !func)
+		return;
+	height = b_t_height(tree);
+	while (lvl <= height)
+		print_level(tree, func, ++lvl);
 }
