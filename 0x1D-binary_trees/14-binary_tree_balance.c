f7ddd24d6002666270c78415d533cc8c5bb8744b
Jose Alvarez de Lugo <845@holbertonschool.com>
2019-12-05 14:18:35 -0500

    [DOCUMENTATION] Add description to function

--- 0x1D-binary_trees/14-binary_tree_balance.c
+++ 0x1D-binary_trees/14-binary_tree_balance.c
@@ -35,8 +35,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int rHeight = 0, lHeight = 0;
 
 	if (!tree)
-		return (0)
-	lHeight = binary_tree_height(tree->left) + 1;
-	rHeight = binary_tree_height(tree->right) + 1;
+		return (0);
+	if (tree->left)
+		lHeight = binary_tree_height(tree->left) + 1;
+	if (tree->right)
+		rHeight = binary_tree_height(tree->right) + 1;
 	return (lHeight - rHeight);
 }
