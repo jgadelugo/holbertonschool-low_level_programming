f843d947d54cb490cebf49be3ae63a34677fc44a
jgadelugo <alvarezdelugo.jose@gmail.com>
2020-01-15 23:25:16 +0000

    [TASK] Binary search

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,68 @@
+#include "search_algos.h"
+
+/**
+  * print_array - prints an array
+  * @arr: array to print
+  * @start: start of print
+  * @end: end of print
+  */
+
+void print_array(int *arr, int start, int end)
+{
+	int i = 0;
+
+	printf("Searching in array: ");
+	while (start <= end)
+	{
+		if (i > 0)
+			printf(", ");
+		i = start++;
+		printf("%d", arr[i++]);
+	}
+	printf("\n");
+}
+
+
+/**
+  * rec_search - recursively search using binary algo
+  * @arr: array to search
+  * @l: left most value
+  * @r: right most value
+  * @val: value to search
+  *
+  * Return: index or -1 if not found
+  */
+
+int rec_search(int *arr, int l, int r, int val)
+{
+	int mid;
+
+	if (r >= l)
+	{
+		mid = l + (r - l) / 2;
+		print_array(arr, l, r, size);
+		if (arr[mid] == val)
+			return (mid);
+		if (arr[mid] > val)
+			return (rec_search(arr, l, mid - 1, val, size));
+		return (rec_search(arr, mid + 1, r, val, size));
+	}
+	return (-1);
+}
+
+
+/**
+  * binary_search - Search for a value in an array of ints using binary search
+  * @array: array to search
+  * @size: of array
+  * @value: Value to search for
+  *
+  * Return: first index where the value is located
+  */
+
+int binary_search(int *array, size_t size, int value)
+{
+	if (array && size)
+		return (rec_search(array, 0, (int)size - 1, value, (int)size));
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -5,7 +5,7 @@
 
 
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
 
 
 #endif /*SEARCH_ALGORITHM*/
