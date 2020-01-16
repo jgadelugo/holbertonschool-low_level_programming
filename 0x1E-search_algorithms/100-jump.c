6a598ff8adf807b3cd79dbaf134e61b63ef9d80c
jgadelugo <alvarezdelugo.jose@gmail.com>
2020-01-16 20:46:26 +0000

    [TASK] Jump algo, ignore the print statements

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,25 +13,23 @@ int jump_search(int *array, size_t size, int value)
 {
 	unsigned int i, jump = sqrt(size);
 
-	for (i = 0; i < size; i += jump)
+	for (i = 0; i < size;)
 	{
 		if (array[i + jump] >= value)
 			break;
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (i + jump < size)
+			i += jump;
+		else
+			break;
 	}
-	if (i < size)
-	{
-		printf("Value checked array[%d] = [%d]\n", i, array[i]);
-		printf("Value found between indexes [%d] and [%d]\n", i, i + jump);
-	}
-	else
-		printf("Value found between indexes [%d] and [%d]\n", i - jump, i);
+	printf("Value checked array[%d] = [%d]\n", i, array[i]);
+	printf("Value found between indexes [%d] and [%d]\n", i, i + jump);
 	for (; i <= (i + jump) && i < size; i++, jump--)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
-	printf("Value checked array[%d] = [%d]\n", i - jump, array[i - jump]);
 	return (-1);
 }
