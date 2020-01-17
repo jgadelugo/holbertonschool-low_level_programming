490b79a9b12af34cceca26421b2777bb8df3ecaf
Jose-AlvarezdeLugo <alvarezdelugo.jose@gmail.com>
2020-01-17 17:21:47 -0500

    [LINTER] Fix styling

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -15,9 +15,9 @@
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
 
 /**
@@ -33,10 +33,10 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
+ 	int n;
+	size_t index;
+ 	struct skiplist_s *next;
+	struct skiplist_s *express;
 } skiplist_t;
 
 int linear_search(int *array, size_t size, int value);
